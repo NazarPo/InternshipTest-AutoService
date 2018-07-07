67a3d4fd9fb1cc6f2b9e15f007e1187b1733f85c
unrenamed <renkonazbka@gmail.com>
2018-07-07 17:41:13 +0300

    added Client class

---
+++ src/person/Client.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Client {
+  private String name;
+
+  public Client(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+}
