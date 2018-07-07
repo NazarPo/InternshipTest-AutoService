c69fbf02810e57bf002a5294e4c49420d4f151be
unrenamed <renkonazbka@gmail.com>
2018-07-07 17:59:06 +0300

    edited AutoService class: added closeBill method with supplementary methods

--- src/company/AutoService.java
+++ src/company/AutoService.java
@@ -1,6 +1,11 @@
 package company;
 
+import company.documents.Bill;
 import company.documents.Journal;
+import company.strategies.DiscountStrategy;
+import person.Client;
+
+import java.util.*;
 
 public class AutoService {
   private Journal servicesLog;
@@ -9,5 +14,41 @@ public class AutoService {
   public AutoService(String name) {
     this.name = name;
     this.servicesLog = new Journal();
+  }
+
+  public Bill createBillFor(Client client) {
+    return new Bill(client);
+  }
+
+  public void closeBill(Bill bill, List<DiscountStrategy> discounts) {
+    bill.calculateTotalServicesSum();
+    applyDiscounts(bill, discounts);
+    servicesLog.addToJournal(bill);
+  }
+
+  private void applyDiscounts(Bill bill, List<DiscountStrategy> discounts){
+    Map<DiscountStrategy, Integer> discountsEffects = new HashMap<>();
+    for(DiscountStrategy discount: discounts){
+      if(check(bill, discount)){
+        Bill clone = bill.clone();
+        clone.applyDiscount(discount);
+        discountsEffects.put(discount, bill.getTotalPrice() - clone.getTotalPrice());
+      }
+    }
+    if(!discountsEffects.isEmpty()){
+      bill.applyDiscount(calculateBestDiscount(discountsEffects));
+    }
+  }
+
+  private DiscountStrategy calculateBestDiscount(Map<DiscountStrategy, Integer> map){
+    Map.Entry<DiscountStrategy, Integer> temp = Collections.max(
+            map.entrySet(),
+            Comparator.comparing(Map.Entry::getValue)
+    );
+    return temp.getKey();
+  }
+
+  private boolean check(Bill bill, DiscountStrategy discount){
+    return bill.getServices().contains(discount.getService()) && discount.isActive() && discount.checkCondition(servicesLog, bill);
   }
 }
